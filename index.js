11fd4210c8d5dda87a8c8d77352856a9692f2f06
jellyware-1 <125736418+jellyware-1@users.noreply.github.com>
2023-07-13 21:01:03 +0100

    fixed errors

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ require('dotenv').config();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-// var cors = require('cors');
-// app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+//  var cors = require('cors');
+//  app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-app.get('/', (req,res)=>{res.sendFile(__dirname + "/public/index.html");console.log("sending file")});
+app.get('/', (req,res)=>{res.sendFile(__dirname + "/public/index.html");});
 
 app.get('/api/whoami', (req, res)=>{
-    console.log(req);
-    res.json({"ipaddress": req.ip, "language": req.rawHeaders[29], "sofware": req.rawHeaders[15]});
+    res.json({"ipaddress": req.ip, "language": req.headers["accept-language"], "sofware": req.headers["user-agent"]});
 });
 
 app.listen(process.env.PORT, ()=>{
